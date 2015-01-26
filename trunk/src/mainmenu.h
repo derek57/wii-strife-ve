327ff8ce8bf8476c8dd5d20e1a856a866550359e
vermillion57 <vermillion57@96a43621-2c30-d8bc-5390-7fa350a2b080>
2015-01-26 14:23:32 +0000

    OOPS... FORGOT TO ADD SEVERAL SOURCE FILE FOR [SVE] MODE

---
+++ trunk/src/mainmenu.h
@@ -0,0 +1,13 @@
+#ifndef __MAINMENU_H__
+#define __MAINMENU_H__
+
+#include "video.h"
+
+#define TESTING_CODE 0
+#define GUI_BACKGROUND_COLOR CONSOLE_BLACK
+#define DEFAULT_FONT_BGCOLOR 0
+#define DEFAULT_FONT_FGCOLOR 7
+#define DEFAULT_FONT_WEIGHT 0
+#define VERSION "3.1"
+
+#endif
